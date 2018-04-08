90ffb5e986dc96277cb886eb06f3de55029659fc
raawaa <wenjie_yu@yeah.net>
2018-04-09 01:31:24 +0800

    搜索结果排版对齐

--- src/sd_music/utils/shower.py
+++ src/sd_music/utils/shower.py
@@ -1,9 +1,11 @@
 
 def show_music(index,music_name,authors,album=''):
-    print(" "+str(index) + "\t" + music_name + "\t" + authors + "\t"  + album)
+    # print(" "+str(index) + "\t" + music_name + "\t" + authors + "\t"  + album)
+    print(" {:^4}\t{:40}\t{:20}\t{:40}".format(index,music_name,authors,album))
 
 def show_title():
-    print("Index\tMusicName\tMusicAuthor\tMusicAlbum")
+    # print("Index\tMusicName\tMusicAuthor\tMusicAlbum")
+    print(" {:^4}\t{:40}\t{:20}\t{:40}".format('Index','Music_Name','Authors','Album'))
 
 def show_out_of_bound():
     print('越界错误')
